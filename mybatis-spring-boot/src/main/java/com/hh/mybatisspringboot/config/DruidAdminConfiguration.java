43792833a4dae2524a9b52878499058d932e9304
chenguoku <cgkhh1996@163.com>
2020-07-22 10:39:54 +0800

    集成druid数据库连接池及druid页面

---
+++ mybatis-spring-boot/src/main/java/com/hh/mybatisspringboot/config/DruidAdminConfiguration.java
@@ -0,0 +1,36 @@
+package com.hh.mybatisspringboot.config;
+
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class DruidAdminConfiguration {
+
+    @Bean
+    public ServletRegistrationBean druidServlet() {
+        ServletRegistrationBean reg = new ServletRegistrationBean();
+        reg.setServlet(new StatViewServlet());
+        reg.addUrlMappings("/druid/*");
+        //reg.addInitParameter("allow", "127.0.0.1");
+        //reg.addInitParameter("deny","");
+        reg.addInitParameter("loginUsername", "hh2016");
+        reg.addInitParameter("loginPassword", "123456");
+        return reg;
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
+    }
+
+}
